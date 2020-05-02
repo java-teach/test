09a1669569687776d34d4c191663baf4208ec954
rl <rl@126.com>
2020-05-02 08:18:30 +0800

    commit a file

---
+++ src/com/rl/lock/A.java
@@ -0,0 +1,16 @@
+/**
+ * 公 司：
+ * 文件名：A
+ * 作 者:Administrator
+ * 时 间:2020/5/2 8:09
+ * 描 述：
+ */
+
+
+package com.rl.lock;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("----------");
+    }
+}
